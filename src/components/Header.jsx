53eef7f0045a343bf4f36602251614ccf52585e6
Nikita-Bari <132455296+Nikita-Bari@users.noreply.github.com>
2025-11-30 23:47:32 +0530

    Change link target from '/' to '/home'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(){
  <header>
 
 <div className="menu">
-  <Link to="/" className="link"><div className="abc-menu">All</div></Link>
+  <Link to="/home" className="link"><div className="abc-menu">All</div></Link>
   <Link to="/electronic"  className="link"><div className="abc-menu">Electronics</div></Link>
   <Link to="/essential"  className="link"><div className="abc-menu">Essential</div></Link>
  <Link to="/cosmetic"  className="link"> <div className="abc-menu">Cosmetics</div></Link>
